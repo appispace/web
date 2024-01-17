7b9d157247e55869459614f3fe8219ac90556c0e
Miguel Araya <miaraya@gmail.com>
2024-01-17 20:08:14 +0700

    change images

--- index.php
+++ index.php
@@ -282,7 +282,7 @@
           <div class="row content">
             <div class="col-md-4" data-aos="fade-right">
               <img
-                src="assets/img/appistories-1.jpg"
+                src="assets/img/appistories-1.png"
                 class="img-fluid"
                 alt=""
                 style="align-self: center"
@@ -291,15 +291,34 @@
             <div class="col-md-8 pt-4" data-aos="fade-up">
               <h3>Appisnaps</h3>
 
-              <p>Share stories, moments, thoughts, feelings – anything really – that brings you joy.  These can be from the simplest of things to life changing events.</p>
+              <p>
+                Share stories, moments, thoughts, feelings – anything really –
+                that brings you joy. These can be from the simplest of things to
+                life changing events.
+              </p>
 
-              <p>With 15 joyful categories to choose from you can spread joy in many ways.  Follow all or select your most joyful ones and your feed will be full of posts that interest you the most.</p>
+              <p>
+                With 15 joyful categories to choose from you can spread joy in
+                many ways. Follow all or select your most joyful ones and your
+                feed will be full of posts that interest you the most.
+              </p>
 
-              <p>Using up to 500 characters and 1 media file you can share your joys with others, leaving them feeling more joyful with every swipe.</p>
+              <p>
+                Using up to 500 characters and 1 media file you can share your
+                joys with others, leaving them feeling more joyful with every
+                swipe.
+              </p>
 
-              <p>Sharing these “snapshots” allows you to brighten someone’s day, open someone to new joy or see how many people have shared in a similar kind of joy.</p>
+              <p>
+                Sharing these “snapshots” allows you to brighten someone’s day,
+                open someone to new joy or see how many people have shared in a
+                similar kind of joy.
+              </p>
 
-              <p>Show yourself, and others, how much genuine goodness we have all around us.</p>
+              <p>
+                Show yourself, and others, how much genuine goodness we have all
+                around us.
+              </p>
             </div>
           </div>
 
@@ -340,10 +359,17 @@
             <div class="col-md-8 pt-5" data-aos="fade-up">
               <h3>Appiacts</h3>
               <p>
-                Acts of kindness happen all the time yet we rarely share those moments.  With Appiacts you can share a time someone made your day (or vice versa) and inspire others to do the same.  Those inspired can comment to share their experience or let you know how your act has impacted them.  Your story is public but the comments are not.
+                Acts of kindness happen all the time yet we rarely share those
+                moments. With Appiacts you can share a time someone made your
+                day (or vice versa) and inspire others to do the same. Those
+                inspired can comment to share their experience or let you know
+                how your act has impacted them. Your story is public but the
+                comments are not.
               </p>
               <p>
-                This is the only place with comments, so you must opt in.  We support a conscious choice about opening yourself up to feedback.
+                This is the only place with comments, so you must opt in. We
+                support a conscious choice about opening yourself up to
+                feedback.
               </p>
             </div>
           </div>
@@ -427,16 +453,16 @@
               </div>
               <div class="swiper-slide">
                 <a
-                  href="assets/img/gallery/gallery-3-2.png"
+                  href="assets/img/gallery/gallery-3.png"
                   class="gallery-lightbox"
                   data-gall="gallery-carousel"
                   ><img
-                    src="assets/img/gallery/gallery-3-2.png"
+                    src="assets/img/gallery/gallery-3.png"
                     class="img-fluid"
                     alt=""
                 /></a>
               </div>
-              <!-- <div class="swiper-slide">
+              <div class="swiper-slide">
                 <a
                   href="assets/img/gallery/gallery-4.png"
                   class="gallery-lightbox"
@@ -446,7 +472,8 @@
                     class="img-fluid"
                     alt=""
                 /></a>
-              </div> -->
+              </div>
+
               <div class="swiper-slide">
                 <a
                   href="assets/img/gallery/gallery-5.png"
@@ -469,7 +496,7 @@
                     alt=""
                 /></a>
               </div>
-              <!-- <div class="swiper-slide">
+              <div class="swiper-slide">
                 <a
                   href="assets/img/gallery/gallery-7.png"
                   class="gallery-lightbox"
@@ -479,7 +506,8 @@
                     class="img-fluid"
                     alt=""
                 /></a>
-              </div> -->
+              </div>
+
               <div class="swiper-slide">
                 <a
                   href="assets/img/gallery/gallery-8.png"
@@ -863,76 +891,7 @@
                 <p>Calgary, Canada</p>
               </div>
             </div>
-            <!--
-            <div class="col-lg-6 form">
-              <form
-                action="forms/contact.php"
-                method="post"
-                role="form"
-                class="php-email-form"
-                data-aos="fade-up">
-                <div class="form-group">
-                  <input
-                    placeholder="Your Name"
-                    type="text"
-                    name="name"
-                    class="form-control"
-                    id="name"
-                    required
-                    />
-                </div>
-                <div class="form-group mt-3">
-                  <input
-                    placeholder="Your Email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    id="email"
-                    required
-                    />
-                </div>
-                <div class="form-group mt-3">
-                  <input
-                    placeholder="Subject"
-                    type="text"
-                    class="form-control"
-                    name="subject"
-                    id="subject"
-                    required
-                    />
-                </div>
-                <div class="form-group mt-3">
-                  <textarea
-                    placeholder="Message"
-                    class="form-control"
-                    name="message"
-                    rows="5"
-                    id="message"
-                    required></textarea>
-                </div>
-                <div class="my-3">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">
-                    Your message has been sent. Thank you!
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button type="submit">Send Message</button>
-                </div>
-              </form>
 
-              <div class="socmed">
-                <ul>
-                  <li><a href="https://www.facebook.com/appispaceapp/"><i class="fa-brands fa-facebook-square"></i></a></li>
-                  <li><a href="https://www.instagram.com/appispace/"><i class="fa-brands fa-instagram-square"></i></a></li>
-                  <li><a href="https://www.linkedin.com/company/appispaceapp/"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                  <li><a href="https://www.youtube.com/channel/UCJYVartqq0-W7kDcai-3m-Q"><i class="fa-brands fa-youtube"></i></a></li>
-                  <li><a href="https://twitter.com/appispace"><i class="fa-brands fa-twitter"></i></a></li>
-                </ul>
-              </div>
-            </div>
-            -->
             <div class="col-lg-6">
               <div class="info">
                 <i class="bx bx-envelope"></i>
@@ -949,100 +908,6 @@
 
     <!-- ======= Footer ======= -->
     <footer id="footer">
-      <!--
-    <div class="footer-newsletter">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-            <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>-->
-      <!-- <div class="footer-top">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-md-6 footer-contact">
-              <h3>Appland</h3>
-              <p>
-                A108 Adam Street <br />
-                New York, NY 535022<br />
-                United States <br /><br />
-                <strong>Phone:</strong> +1 5589 55488 55<br />
-                <strong>Email:</strong> info@example.com<br />
-              </p>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Useful Links</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Home</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">About us</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Services</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Terms of service</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Privacy policy</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Our Services</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Web Design</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Web Development</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Product Management</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Marketing</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Graphic Design</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Our Social Networks</h4>
-              <p>
-                Cras fermentum odio eu feugiat lide par naso tierra videa magna
-                derita valies
-              </p>
-              <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"
-                  ><i class="bx bxl-instagram"></i
-                ></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> -->
       <div class="container py-4">
         <div class="row align-items-center justify-content-between">
           <div class="col-lg-6">
@@ -1137,7 +1002,7 @@
             <strong><span>Privacy Policy</span></strong></a
           >
           <span
-            >© Copyright <strong><span>Appispace</span></strong> 2022 • All
+            >© Copyright <strong><span>Appispace</span></strong> 2024 • All
             Rights Reserved</span
           >
         </div>
